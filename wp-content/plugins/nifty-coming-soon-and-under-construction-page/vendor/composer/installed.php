74a9b2777341cc46e060c6c6125d8944565964a6
Emad Saeed <hello@emadsaeed.com>
2022-08-04 18:41:52 +0100

    Alexmed backup

---
+++ wp-content/plugins/nifty-coming-soon-and-under-construction-page/vendor/composer/installed.php
@@ -0,0 +1,41 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => 'af67a24fcc2ba01d680d5895284d7e2d40d4f4f3',
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => 'af67a24fcc2ba01d680d5895284d7e2d40d4f4f3',
+            'dev_requirement' => false,
+        ),
+        'ernilambar/wp-admin-notice' => array(
+            'pretty_version' => '1.0.0',
+            'version' => '1.0.0.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../ernilambar/wp-admin-notice',
+            'aliases' => array(),
+            'reference' => '1fd14ff6d79994750d25b4f529284a6942e117eb',
+            'dev_requirement' => false,
+        ),
+        'wptt/webfont-loader' => array(
+            'pretty_version' => 'v1.1.2',
+            'version' => '1.1.2.0',
+            'type' => 'package',
+            'install_path' => __DIR__ . '/../wptt/webfont-loader',
+            'aliases' => array(),
+            'reference' => '780e564bf0554c419ef447c2b5b768b497ecee40',
+            'dev_requirement' => false,
+        ),
+    ),
+);
